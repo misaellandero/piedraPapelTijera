06e70604cddae6d18bfa0136965beb032bd1496d
misaellandero <misaellandero@icloud.com>
2019-12-07 13:19:18 -0600

    Initial Commit

---
+++ piedraPapelTijera/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  piedraPapelTijera
+//
+//  Created by Francisco Misael Landero Ychante on 07/12/19.
+//  Copyright © 2019 Francisco Misael Landero Ychante. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
